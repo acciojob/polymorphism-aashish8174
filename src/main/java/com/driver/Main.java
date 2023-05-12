6f9a3ef43bfa11bc37d2d8be36494676c6fac7a0
aashish kumar yadav <aashish8174@gmail.com>
2023-05-12 11:35:59 +0530

    polymorphism used

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
 public class Main {
+      //public static void main(String args[]){
+          Product p = new Product();
+          int prod1 = p.product(1,2);
+          int prod2=p.product(3,4,5);
+          double prod3 = p.product(6,7);
+    //  }
 
+    public class Product{
+        public  int product(int x , int y){
+            return x*y;
+        }
+        public  int product(int x,int y,int z){
+            return x*y*z;
+        }
+        public  double product(double x,double y){
+            return (double)x*y;
+        }
+    }
 }
