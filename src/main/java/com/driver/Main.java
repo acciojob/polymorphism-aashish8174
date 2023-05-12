def060a5194c4fefcdfb7e003b8a596df61c3fdf
aashish kumar yadav <aashish8174@gmail.com>
2023-05-12 11:47:03 +0530

    polymorphism methodes are created

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,14 +1,14 @@
 package com.driver;
 
 public class Main {
-      //public static void main(String args[]){
+
           Product p = new Product();
           int prod1 = p.product(1,2);
           int prod2=p.product(3,4,5);
           double prod3 = p.product(6,7);
-    //  }
 
-    public class Product{
+
+    public static class Product{
         public  int product(int x , int y){
             return x*y;
         }
